dcde123dc9dd41b17a88dca40643d40fc16a7ebe
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-01-18 16:30:15 +0530

    Add default layout + navbar + footer component

---
+++ src/app/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="footer py-4 px-8">
+            footer
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+import SearchImage from '../../common/images/search.svg';
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+      <div className="flex justify-between items-center py-4 px-8">
+        <div className="logo w-64">
+          <Link className="text-3xl font-[--heading-font]" href={'#'}>Byte Height</Link>
+        </div>
+        <div className="menu w-full">
+          <ul className="flex text-left text-md">
+            <li className="me-4"><Link className="p-2 hover:border-b-2 hover:text-[#FFB0B0] border-[#FFB0B0]" href={'#'}>Beauty</Link></li>
+            <li className="me-4"><Link className="p-2 hover:border-b-2 hover:text-[#729762] border-[#729762]" href={'#'}>Lifestyle</Link></li>
+            <li className="me-4"><Link className="p-2 hover:border-b-2 hover:text-[#FCDC94] border-[#FCDC94]" href={'#'}>Technology</Link></li>
+            <li className="me-4"><Link className="p-2 hover:border-b-2 hover:text-[#E3F4F4] border-[#E3F4F4]" href={'#'}>Travel</Link></li>
+            <li className=""><Link className="p-2 hover:border-b-2 hover:text-[#FEBE8C] border-[#FEBE8C]" href={'#'}>Worldwide</Link></li>
+          </ul>
+        </div>
+        <div className="subscribe text-right">
+          <ul className="flex items-center">
+            <li><Image className="hover:cursor-pointer" src={SearchImage} width={60} height={60} alt="search-icon"/></li>
+            <li className="ms-5"><Link className="px-6 py-2 font-bold rounded-lg text-[--foreground] bg-[#AB886D] hover:opacity-90" href={'#'}>Subscribe</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </>
+  );
+};
+
+export default Navbar;
