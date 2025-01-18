1c1f2fa7d2b91ccc9d9365ee8753166855ace20e
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-01-18 20:33:02 +0530

    Add copyright and author name

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className="footer py-4 px-8">
-            footer
+        <div className="footer container mx-auto py-4">
+          <div className="copyright border-t border-gray-600 flex justify-between py-3">
+            <p>All Rights Reserved &copy; 2025</p>
+            <p>Managed By: <Link href={'#'}>Gurpreet Gumber</Link></p>
+          </div>
         </div>
       </footer>
     </>
