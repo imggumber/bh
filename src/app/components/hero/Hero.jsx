5abc19e209cc5d5b33e33a2777d536838a7890ea
Gurpreet Gumber <imgurpreetgumber@gmail.com>
2025-01-18 21:23:14 +0530

    Create hero component

---
+++ src/app/components/hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Category from '../category/Category';
+import Link from 'next/link';
+import BackgroundImg from '../../common/images/bg_1.jpg';
+
+let style = {
+  'background': `url(${BackgroundImg.src})`,
+  'width': '100%',
+  'height': '100%',
+  'backgroundRepeat': 'no-repeat',
+  'backgroundPosition': 'top'
+}
+
+
+const Hero = () => {
+  return (
+    <>
+    <div style={style} className="hero h-full rounded-lg flex items-end">
+      <div className="title__category p-4">
+        <div className="title">
+          <Category />
+          <Link href={'#'} className='mt-4 mb-32 block'>
+            <h1>Popular Lifestyle with Fashion & Modeling</h1>
+          </Link>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
